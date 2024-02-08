ebb387408bcf7917b4bd3bee03c52853e840825c
dariokempf <dario.kempf@edu.vs.ch>
2024-02-08 14:10:08 +0100

    folder images / footer.blade.php created

---
+++ oum_webapp/resources/views/layouts/footer.blade.php
@@ -0,0 +1,24 @@
+<div class="row row--style-spare row--height-default row--vertical-align-center row--background-type-color row--width-contained row--logos-2a row--layout-logos-2a js-row" data-rowtype="logos-2a">
+    <div class="row--scroll-effect--container"></div>
+    <div class="row__container js-row-container">
+        <!-- Hier kommen deine Logos und Links hin -->
+    </div>
+</div>
+
+<div class="row row--style-accent row--height-thin row--vertical-align-center row--background-type-color row--width-medium row--footer-2 js-row" data-rowtype="footer-2">
+    <div class="row--scroll-effect--container"></div>
+    <div class="row__container js-row-container">
+        <div id="page-zones__footer-widgets__64d1cd43802e1-widgets__64d1cd4391ccb" data-name="companybrand" class="widget widget--zone-widget js-widget" data-uniqueid="46qNlz62NQ5Bz6e" data-parentuniqueid="2wtFCs8KSDVHg5Q">
+            <!-- Hier kommt dein Firmenlogo hin -->
+        </div>
+        <div id="page-zones__footer-widgets__64d1cd43802e1-widgets__64d1cd4398780" data-name="contentgroup" class="widget widget--zone-widget js-widget" data-uniqueid="dzHLJIlbhkjLqTG" data-parentuniqueid="2wtFCs8KSDVHg5Q">
+            <!-- Hier kommt der Textinhalt des Footers hin -->
+        </div>
+        <div id="page-zones__footer-widgets__64d1cd43802e1-widgets__64d1cd43a286d" data-name="contact" class="widget widget--zone-widget js-widget" data-uniqueid="6Otj3WaG3Rwenu1" data-parentuniqueid="2wtFCs8KSDVHg5Q">
+            <!-- Hier kommen deine Kontaktdaten und Social-Media-Links hin -->
+        </div>
+        <div id="page-zones__footer-widgets__64d1cd43802e1-widgets__64d1cd43b9bb7" data-name="policypages" class="widget widget--zone-widget js-widget" data-uniqueid="lXbsTXI6ZqjZXoq" data-parentuniqueid="2wtFCs8KSDVHg5Q">
+            <!-- Hier kommen ggf. Links zu Datenschutzrichtlinien oder ähnliches hin -->
+        </div>
+    </div>
+</div>
