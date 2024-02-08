37ed84c56dde5f2f367030835aa43b8a0a198b3e
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-02-08 15:54:28 +0100

    did sponsores and design

--- oum_webapp/resources/views/dashboard.blade.php
+++ oum_webapp/resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
+    
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
@@ -22,7 +22,13 @@
                 </div>
 
             </div>
+            
         </div>
-        @include('layouts.footer')
+        
     </div>
+    <footer>
+    @include('layouts.footer')
+    </footer>
+   
 </x-app-layout>
+
--- oum_webapp/resources/views/layouts/footer.blade.php
+++ oum_webapp/resources/views/layouts/footer.blade.php
@@ -1,27 +1,49 @@
-<link rel="stylesheet" href="app.css">
-<div class="bg-white sticky bottom-0 shadow" class="footer">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="app.css">
+</head>
+<body>
+
+<div class="bg-white sticky bottom-0 shadow footer">
     <div class="container mx-auto py-4 px-5">
-        <div class="row">
-            <!-- Große Bilder in der ersten Reihe -->
-            <div style="display: flex;">
-                <img src="{{ asset('img/conc.jpg') }}" class="img-fluid mr-20 "  alt="Bild 1">
-                <img src="{{ asset('img/mob.png') }}" class="img-fluid ml-20" alt="Bild 2">
-            </div>
-
-
-
-
-
-
-            <!-- Kleine Bilder in der zweiten Reihe -->
-            <!-- <div class="col-lg-3 col-md-3 col-sm-6">
-                <img src="{{ asset('img/logo.png') }}" class="img-fluid" alt="Bild 3">
-            </div> -->
-            <div>
-                <img src="{{ asset('img/arxada.png') }}" class="img-fluid" alt="Bild 4">
-                <img src="{{ asset('img/unicnt.png') }}" class="img-fluid" alt="Bild 5">
-                <img src="{{ asset('img/wkb.png') }}" class="img-fluid" alt="Bild 6">
-            </div>
-        </div>
+        <table class="mx-auto">
+            <tr>
+                <!-- Erste Reihe -->
+                <td class="text-center">
+                    <p class="font-bold mb-2">Hauptsponsor 1</p>
+                    <img src="{{ asset('img/conc.jpg') }}" class="img-fluid" alt="concordia" width="700px">
+                </td>
+                <td class="text-center">
+                    <p class="font-bold mb-2">Hauptsponsor 2</p>
+                    <img src="{{ asset('img/mob.png') }}" class="img-fluid ml-20" alt="mobiliar" width="700px">
+                </td>
+            </tr>
+            <tr>
+                <!-- Zweite Reihe -->
+                <td class="text-center">
+                    <p class="font-bold mb-2">Arxada</p>
+                    <img src="{{ asset('img/arxada.png') }}" class="img-fluid ml-0" alt="Arxada" width="500">
+                </td>
+                <td class="text-center">
+                    <p class="font-bold mb-2">Unihockey Center.ch</p>
+                    <img src="{{ asset('img/unicnt.png') }}" class="img-fluid ml-5" alt="Bild Unihockey Center.ch" width="500">
+                </td>
+                <td class="text-center">
+                    <p class="font-bold mb-2">WKB</p>
+                    <img src="{{ asset('img/wkb.png') }}" class="img-fluid ml-10" alt="Bild WKB" width="500">
+                </td>
+                <td class="text-center">
+                    <p class="font-bold mb-2">Lettermann</p>
+                    <img src="{{ asset('img/lettermann.png') }}" class="img-fluid ml-20" alt="lettermann" width="500" >
+                </td>
+            </tr>
+        </table>
     </div>
 </div>
+
+</body>
+</html>
