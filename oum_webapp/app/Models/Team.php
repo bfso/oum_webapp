47e270f466b02881437c6c958eb63d1aa9999dcd
Luanasani <luanasani48@gmail.com>
2024-02-22 15:58:50 +0100

    navbar Update

--- oum_webapp/app/Models/Team.php
+++ oum_webapp/app/Models/Team.php
@@ -3,14 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Team extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'category_id'];
 
-    public function players()
+    public function category()
     {
-        return $this->belongsToMany(Player::class, 'player_teams', 'team_id', 'player_id');
+        return $this->belongsTo(Category::class);
     }
 }
--- oum_webapp/resources/views/gameoperation.blade.php
+++ oum_webapp/resources/views/gameoperation.blade.php
@@ -9,26 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                <a href="{{ route('gameoperation', ['league' => 'herrenA']) }}">Herren A</a>
-                <a href="{{ route('gameoperation', ['league' => 'herrenB']) }}">Herren B</a>
-                <a href="{{ route('gameoperation', ['league' => 'damen']) }}">Damen</a>
-                <a href="{{ route('gameoperation', ['league' => 'junioren']) }}">Junioren</a>
-                <a href="{{ route('gameoperation', ['league' => 'juniorenA']) }}">Junioren A</a>
-                <a href="{{ route('gameoperation', ['league' => 'juniorenB']) }}">Junioren B</a>
-                <a href="{{ route('gameoperation', ['league' => 'playoffs']) }}">Playoffs</a>
-                <a href="{{ route('gameoperation', ['league' => 'cup']) }}">Cup</a>
+                    <!-- Liste der Links für verschiedene Ligen -->
+                    <div class="flex space-x-4">
+                        <a href="{{ route('gameoperation', ['league' => 'herrenA']) }}" class="text-blue-500 hover:text-blue-700">Herren A</a>
+                        <a href="{{ route('gameoperation', ['league' => 'herrenB']) }}" class="text-blue-500 hover:text-blue-700">Herren B</a>
+                        <!-- Weitere Links für andere Ligen hinzufügen -->
+                    </div>
 
+                    <!-- Anzeige der Daten für die ausgewählte Liga -->
+                    <div class="mt-4">
+                        @foreach ($data as $item)
+                            <p>{{ $item->name }}</p>
+                            <!-- Weitere Felder anzeigen, je nach Bedarf -->
+                        @endforeach
 
-
-    <!-- Weitere Liga-Links hier -->
-</div>
-
-@foreach ($data as $item)
-    <!-- Hier zeigst du die Daten entsprechend an -->
-@endforeach
-
-
+                    </div>
                 </div>
             </div>
         </div>
