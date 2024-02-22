81bf649c7afa9af3af3cd5abd7a469b1fa0fb77d
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-02-22 13:49:59 +0100

    changed logo

--- oum_webapp/resources/views/layouts/footer.blade.php
+++ oum_webapp/resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dashboard</title>
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
@@ -13,32 +13,26 @@
         <table class="mx-auto">
             <tr>
                 <!-- Erste Reihe -->
-                <td class="text-center">
-                    <p class="font-bold mb-2">Hauptsponsor 1</p>
-                    <img src="{{ asset('img/conc.jpg') }}" class="img-fluid" alt="concordia" width="700px">
+                <td class="text-center" style="padding-bottom: 20px;">
+                    <img src="{{ asset('img/conc.jpg') }}" class="img-fluid mb-2" alt="concordia" style="width: 400px; height: auto; margin-right: 100px;">
                 </td>
-                <td class="text-center">
-                    <p class="font-bold mb-2">Hauptsponsor 2</p>
-                    <img src="{{ asset('img/mob.png') }}" class="img-fluid ml-20" alt="mobiliar" width="700px">
+                <td class="text-center" style="padding-bottom: 20px;">
+                    <img src="{{ asset('img/mob.png') }}" class="img-fluid mb-2" alt="mobiliar" style="width: 400px; height: auto;">
                 </td>
             </tr>
             <tr>
                 <!-- Zweite Reihe -->
-                <td class="text-center">
-                    <p class="font-bold mb-2">Arxada</p>
-                    <img src="{{ asset('img/arxada.png') }}" class="img-fluid ml-0" alt="Arxada" width="500">
+                <td class="text-center" style="padding-top: 10px;">
+                    <img src="{{ asset('img/arxada.png') }}" class="img-fluid mb-2" alt="Arxada" style="width: 250px; height: auto; margin-right: 0px;">
                 </td>
-                <td class="text-center">
-                    <p class="font-bold mb-2">Unihockey Center.ch</p>
-                    <img src="{{ asset('img/unicnt.png') }}" class="img-fluid ml-5" alt="Bild Unihockey Center.ch" width="500">
+                <td class="text-center" style="padding-top: 10px;">
+                    <img src="{{ asset('img/unicnt.png') }}" class="img-fluid mb-2" alt="Bild Unihockey Center.ch" style="width: 250px; height: auto; margin-right: 100px;">
                 </td>
-                <td class="text-center">
-                    <p class="font-bold mb-2">WKB</p>
-                    <img src="{{ asset('img/wkb.png') }}" class="img-fluid ml-10" alt="Bild WKB" width="500">
+                <td class="text-center" style="padding-top: 10px;">
+                    <img src="{{ asset('img/wkb.png') }}" class="img-fluid mb-2" alt="Bild WKB" style="width: 250px; height: auto; margin-right: 20px; margin-right: 100px;">
                 </td>
-                <td class="text-center">
-                    <p class="font-bold mb-2">Lettermann</p>
-                    <img src="{{ asset('img/lettermann.png') }}" class="img-fluid ml-20" alt="lettermann" width="500" >
+                <td class="text-center" style="padding-top: 10px;">
+                    <img src="{{ asset('img/lettermann.png') }}" class="img-fluid mb-2" alt="Bild 7" style="width: 250px; height: auto; margin-right: 0px; ">
                 </td>
             </tr>
         </table>
