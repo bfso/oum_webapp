d6a56d2cb8b2fa7b1b4cf45ffe704055d56ab902
Luanasani <luanasani48@gmail.com>
2024-02-22 14:16:35 +0100

    history Page Update

---
+++ oum_webapp/resources/views/history.blade.php
@@ -0,0 +1,59 @@
+
+<x-app-layout>
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Geschichte') }}
+        </h2>
+    </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <h1>Der Ursprung</h1><br>
+                <p>Ums Jahr 2000 herum entwickelte sich die Unihockey Situation im Oberwallis etwas negativ. Die Oberwalliser Unihockeyvereine bekundeten Mühe neue Mitglieder für die Mannschaften zu finden. 
+                Kilian Karlen und Olivier Salzgeber diskutierten nach einem Training lange über dieses Thema und sie fragen sich, warum in der Region Bern x neue Vereine / Mannschaften aus dem Boden gestampft werden und im Oberwallis das pure Gegenteil eintrifft.
+                Eine Regionale Meisterschaft, wie sie im Raum Bern besteht, gabs im Oberwallis bis dahin nicht. 
+                Deshalb spielten die Teams der UHC Visper Lions und UHC Naters - Brig jeweils alleine in einer Gruppe, was somit lange Turnierreisen (Bern / Westschweiz) mit sich brachte, was ein enormer Mehraufwand für diese Vereine bedeutete. Vielleicht war das ein Grund, warum sich keine Vereine zur SUHV Meisterschaft anmeldeten.
+                Jedenfalls wollte man damals versuchen eine Regionale Meisterschaft auf die Beine zu stellen und frage den Vorstand der UHC Visper Lions zur Hilfe an. 
+                Dieser hatte damals jedoch selber genug Probleme den eigenen Verein am Leben zu erhalten und konnten somit lediglich "Unterstützung" beim Projekt anbieten. So geriet die Idee von damals in Vergessenheit.</p>
+                <br>
+                <h1>Initiative</h1><br>
+                <p>Nach 6 Jahren im Vorstand der UHC Visper Lions hat sich Olivier Salzgeber entschlossen, sich dem Thema "Regionale Unihockey Meisterschaft" zu widmen. Im Frühling 2005 erstellte er eine Website (www.reivilo.ch), die das Oberwallis zum Thema "Regionale Unihockey Meisterschaft" informieren sollte. Kaum war dieser Entwurf fertig, meldete sich Zihlmann Peter bei den UHC Visper Lions und dem UHC Naters - Brig und präsentierte sein Vorhaben (Walliser Unihockey Verband). Er plante somit dasselbe wie wir, jedoch etwas im grösseren Rahmen. Dies hat wohl damit zu tun, dass im Unterwallis bereits eine Regionale Meisterschaft besteht, die vom UHC Sierre Challenge organisiert wird. Um den Gedanken Walliser Unihockey Verband nicht zu vergessen, machten wir uns daran, so rasch als möglich die Regionale Meisterschaft im Oberwallis auf die Beine zu stellen, damit man evtl. 
+                später zum Walliser Unihockey Verband übergehen kann. <br>
+                Unter der Mithilfe von den UHC Visper Lions , UHC Ibex Grächen und UHC Naters - Brig schrieben wir Mitte Oktober 2005 170 Oberwalliser Vereine an und sendeten 90 E-Mails an Vereinsverantwortlich mit der Bitte, die Umfrage auf der Homepage www.reivilo.ch auszufüllen. 26 Umfragen wurden dann schliesslich ausgefüllt. Nach der Analyse der Umfrage war für uns klar, dass wir die Regionale Meisterschaft aufbauen wollen.
+                Wie es aber so oft ist, sagen viele jeweils "Ja", wenn es dann aber definitiv so weit ist, macht niemand mehr mit. Somit konnten wir uns natürlich noch nicht auf der sicheren Seite wägen.
+                Wir schrieben daraufhin alle, die die Umfrage ausgefüllt haben, erneut an und luden diese zur Informationsveranstaltung am 08.04.2006 ein. An dieser waren 11 interessierte Parteien anwesend.</p>
+                <br>
+                <h1>Das Resultat</h1><br>
+                <p>Die Gründung fand am 19. Mai 2006 im Sportcenter Olympica statt, wobei 13 Gründungsmitglieder gezählt werden konnten.</p>
+                <br>
+                <h1>Gründungsmitglieder</h1><br>
+                <p>
+                Blacknosesheep
+                Zermatt Alpine Players
+
+                FB Dynamo Tärbinu
+                UHC Visper Lions
+
+                Sportclub MGB
+                UHC Naters-Brig
+
+                Sportverein Eggerberg
+                Ibex Grächen
+
+                STV Baltschieder
+                Embd Devils
+
+                Turnverein Leuk-Susten
+                UHC Bürchen
+
+                UHC Bitsch
+                </p>
+
+            </div>
+                
+            </div>
+        </div>
+    </div>
+    
+</x-app-layout>
