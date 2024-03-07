4519b53154c6528ca421f9d31ef695566db80893
janbBodenmann <jan.bodenmann2@edu.vs.ch>
2024-03-07 14:53:24 +0100

    form

--- oum_webapp/resources/views/layouts/navigation.blade.php
+++ oum_webapp/resources/views/layouts/navigation.blade.php
@@ -12,7 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-customColor4 dark:text-customColor4">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-customColor6 dark:text-customColor4">
                         {{ __('Home') }}
                     </x-nav-link>
                     <x-nav-link :href="route('edit')" :active="request()->routeIs('edit')" class="text-customColor4 dark:text-customColor4">
