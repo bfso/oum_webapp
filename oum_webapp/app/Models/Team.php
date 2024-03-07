482714a0f05c539e5bd096b454833017bc6fba05
MarioB06 <mbozic2006@gmail.com>
2024-03-07 13:49:45 +0100

    bugfix

--- oum_webapp/app/Models/Team.php
+++ oum_webapp/app/Models/Team.php
@@ -6,18 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-<<<<<<< HEAD
-    use HasFactory;
-
     protected $fillable = ['name', 'category_id'];
-
-    public function category()
-    {
-        return $this->belongsTo(Category::class);
-    }
-=======
-    protected $fillable = ['name', 'category_id'];
->>>>>>> origin/features/19-navbar_gameoperations
 
     public function category()
     {
