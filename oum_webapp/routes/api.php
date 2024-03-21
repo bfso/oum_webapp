7696cdd2eb9ae1d1f8cebbb2c896b83ca1a3ec53
MarioB06 <mbozic2006@gmail.com>
2024-03-21 15:32:45 +0100

    token function

--- oum_webapp/routes/api.php
+++ oum_webapp/routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Result;
 use App\Models\Team;
+use App\Http\Controllers\authController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,30 +16,38 @@ use App\Models\Team;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::middleware('auth:sanctum')->post('/results', function (Request $request) {
-   
-    $user = $request->user();
-    
 
-    
+
+
+
+
+
+Route::post('v1/login', [authController::class, 'signin']);
+
+Route::middleware('auth:sanctum')->post('/results', function (Request $request) {
+
+    $user = $request->user();
+
+
+
     $requestData = $request->validate([
         'team_id' => 'required|integer|exists:teams,id',
         'opponent' => 'required|string',
         'goals' => 'required|integer',
         'goals_conceded' => 'required|integer',
-        
+
     ]);
 
-    
+
     $result = Result::create([
         'team_id' => $requestData['team_id'],
         'opponent' => $requestData['opponent'],
         'goals' => $requestData['goals'],
         'goals_conceded' => $requestData['goals_conceded'],
-        
+
     ]);
 
-    
+
 
     return response()->json(['message' => 'Result submitted successfully'], 201);
 });
@@ -46,7 +56,7 @@ Route::middleware('api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('api')->get('/teams', function () {
+Route::middleware('auth:sanctum')->get('/teams', function () {
     $teams = Team::all();
     return response()->json([
         'message' => 'These are the available Teams',
@@ -67,8 +77,13 @@ Route::post('/login', function (Request $request) {
 
     if (Auth::attempt(['email' => $email, 'password' => $password])) {
         $user = Auth::user();
+
+        $success['token'] = $user->createToken('MyAuthApp')->plainTextToken;
+        $success['name'] = $user->name;
+
         return response()->json([
             'message' => 'Authentication successful',
+            'token' => $success['token'],
             'username' => $user->name,
             'code' => '200'
         ], 200);
@@ -78,5 +93,5 @@ Route::post('/login', function (Request $request) {
             'code' => '401'
         ], 401);
     }
-    
+
 });
