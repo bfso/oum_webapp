a5a1f90bcffaa549ab2dc76a6737efedaee38590
MarioB06 <mbozic2006@gmail.com>
2024-03-26 11:39:16 +0100

    api for games

--- oum_webapp/routes/api.php
+++ oum_webapp/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Game;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Result;
@@ -84,6 +85,80 @@ Route::middleware('auth:sanctum')->get('/v1/players/{id}', function ($id) {
         'code' => 200
     ], 200);
 });
+
+Route::middleware('auth:sanctum')->get('/v1/unplayed-games', function () {
+    $unplayedGames = Game::whereNull('team_1_score')
+        ->whereNull('team_2_score')
+        ->get();
+
+    return response()->json([
+        'message' => 'Unplayed games retrieved successfully',
+        'unplayed_games' => $unplayedGames,
+        'code' => 200
+    ]);
+});
+
+Route::middleware('auth:sanctum')->post('/v1/games/{id}/result', function ($id, Request $request) {
+    $game = Game::find($id);
+
+    if (!$game) {
+        return response()->json([
+            'message' => 'Spiel nicht gefunden',
+            'code' => 404
+        ], 404);
+    }
+
+    // Ergebnis des Spiels aktualisieren
+    $game->update([
+        'team_1_score' => $request->team_1_score,
+        'team_2_score' => $request->team_2_score,
+    ]);
+
+    // Spielinformationen abrufen
+    $team1Id = $game->team_1_id;
+    $team2Id = $game->team_2_id;
+    $team1Score = $game->team_1_score;
+    $team2Score = $game->team_2_score;
+
+    // Teams abrufen
+    $team1 = Team::find($team1Id);
+    $team2 = Team::find($team2Id);
+
+    // Punkte berechnen und Teams aktualisieren
+    if ($team1Score > $team2Score) {
+        $team1->wins += 1;
+        $team2->loses += 1;
+        $team1->points += 3;
+    } elseif ($team1Score < $team2Score) {
+        $team1->loses += 1;
+        $team2->wins += 1;
+        $team2->points += 3;
+    } else {
+        $team1->draws += 1;
+        $team2->draws += 1;
+        $team1->points += 1;
+        $team2->points += 1;
+    }
+
+    // Weitere Statistiken aktualisieren
+    $team1->games += 1;
+    $team1->goals += $team1Score;
+    $team1->goals_conceded += $team2Score;
+
+    $team2->games += 1;
+    $team2->goals += $team2Score;
+    $team2->goals_conceded += $team1Score;
+
+    // Teams speichern
+    $team1->save();
+    $team2->save();
+
+    return response()->json([
+        'message' => 'Ergebnis erfolgreich aktualisiert und Teams aktualisiert',
+        'code' => 200
+    ], 200);
+});
+
 
 
 // Route::middleware('auth:sanctum')->post('/results', function (Request $request) {
